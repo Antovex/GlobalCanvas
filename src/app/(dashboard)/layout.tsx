ba5a0781810c61d080b39e5557ec84f649dab88f
Antarin Ghosal <antaringhosal@gmail.com>
2025-07-08 03:25:19 +0530

    Enhance Dashboard layout with custom scrollbar and role-based menu item visibility

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,7 +10,7 @@ export default function DashboardLayout({
 }>) {
     return <div className="h-screen flex">
         {/* LEFT */}
-        <div className="w-[14%] m:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <div className="w-[14%] m:w-[8%] lg:w-[16%] xl:w-[14%] p-4 overflow-y-auto custom-scrollbar">
             <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
             <Image src="/logo.png" alt="Logo" width={32} height={32} />
             <span className="hidden lg:block font-bold">GlobalCanvas</span>
@@ -18,7 +18,7 @@ export default function DashboardLayout({
             <Menu />
         </div>
         {/* RIGHT */}
-        <div className="w-[86%] m:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <div className="w-[86%] m:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-y-scroll">
             <Navbar />
             {children}
         </div>
