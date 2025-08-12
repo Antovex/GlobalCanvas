93d01fd0a59e665acd71030114d1961589d018a0
Antarin Ghosal <antaringhosal@gmail.com>
2025-08-13 01:46:14 +0530

    Refactor Students page to use Prisma and DB to fetch data; Implement search functionality with name in both Teacher list and Student list

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -5,7 +5,7 @@ import TableSearch from "@/components/TableSearch";
 import { role, teachersData } from "@/lib/data";
 import { prisma } from "@/lib/prisma";
 import { ITEM_PER_PAGE } from "@/lib/settings";
-import { Class, Subject, Teacher } from "@prisma/client";
+import { Class, Prisma, Subject, Teacher } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -115,7 +115,11 @@ const renderRow = (item: TeacherList) => (
                     //         height={16}
                     //     />
                     // </button>
-                    <FormModal table="teacher" type="delete" id={Number(item.id)} /> //id={item.id}
+                    <FormModal
+                        table="teacher"
+                        type="delete"
+                        id={item.id}
+                    />
                 )}
             </div>
         </td>
@@ -131,8 +135,33 @@ const TeacherListPage = async ({
 
     const p = page ? parseInt(page) : 1;
 
+    // URL PARAMS CONDITIONS
+    const query: Prisma.TeacherWhereInput = {};
+
+    if (queryParams) {
+        for (const [key, value] of Object.entries(queryParams)) {
+            if (value !== undefined) {
+                switch (key) {
+                    case "classId":
+                        query.lessons = {
+                            some: {
+                                classId: parseInt(value),
+                            },
+                        };
+                        break;
+                    case "search":
+                        query.name = { contains: value, mode: "insensitive" };
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
     const [data, count] = await prisma.$transaction([
         prisma.teacher.findMany({
+            where: query,
             include: {
                 subjects: true,
                 classes: true,
@@ -140,11 +169,10 @@ const TeacherListPage = async ({
             take: ITEM_PER_PAGE,
             skip: ITEM_PER_PAGE * (p - 1),
         }),
-        prisma.teacher.count(),
+        prisma.teacher.count({
+            where: query,
+        }),
     ]);
-
-
-    console.log(count);
 
     return (
         <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
