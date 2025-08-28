002f4cb6f9669ae063027e15ec4e30f8d59c6cf9
Antarin Ghosal <antaringhosal@gmail.com>
2025-08-28 14:00:01 +0530

    Add react-toastify dependency and implement create/update subject actions

---
+++ src/lib/actions.ts
@@ -0,0 +1,57 @@
+"use server";
+
+import {
+    SubjectSchema,
+} from "./formValidationSchemas";
+import { prisma } from "./prisma";
+
+type CurrentState = { success: boolean; error: boolean };
+
+export const createSubject = async (
+    currentState: CurrentState,
+    data: SubjectSchema
+) => {
+    try {
+        await prisma.subject.create({
+            data: {
+                name: data.name,
+                teachers: {
+                    connect: data.teachers.map((teacherId) => ({
+                        id: teacherId,
+                    })),
+                },
+            },
+        });
+
+        // revalidatePath("/list/subjects");
+        return { success: true, error: false };
+    } catch (err) {
+        console.error("Error creating subject:", err);
+        return { success: false, error: true };
+    }
+};
+
+export const updateSubject = async (
+    currentState: CurrentState,
+    data: SubjectSchema
+) => {
+    try {
+        await prisma.subject.update({
+            where: {
+                id: data.id,
+            },
+            data: {
+                name: data.name,
+                teachers: {
+                    set: data.teachers.map((teacherId) => ({ id: teacherId })),
+                },
+            },
+        });
+
+        // revalidatePath("/list/subjects");
+        return { success: true, error: false };
+    } catch (err) {
+        console.error(err);
+        return { success: false, error: true };
+    }
+};
